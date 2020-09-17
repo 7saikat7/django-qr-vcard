b6b950bc85625c66d7455f3192f98677f07461ba
Sam <samysaad01@gmail.com>
2020-09-17 17:10:39 +0200

    Generation of Vcard model.

--- qr_vcard/models/vcard.py
+++ qr_vcard/models/vcard.py
@@ -6,13 +6,60 @@ class VCard(models.Model):
     """
     Vcard model.
     """
-    title = models.CharField(
-        _("Title"),
+    organization = models.CharField(
+        _("Organization"),
         blank=True,
         max_length=150,
         default="",
     )
-
+    name_first = models.CharField(
+        _("First name"),
+        blank=False,
+        max_length=150,
+        default="",
+    )
+    name_last = models.CharField(
+        _("Last name"),
+        blank=False,
+        max_length=150,
+        default="",
+    )
+    mobile_pers = models.CharField(
+        _("Personal phone number"),
+        blank=True,
+        max_length=50,
+        default="",
+    )
+    mobile_pro = models.CharField(
+        _("Professional phone number"),
+        blank=True,
+        max_length=50,
+        default="",
+    )
+    mail_pers = models.EmailField(
+        _("Personal email"),
+        blank=True,
+        max_length=254,
+        default="",
+    )
+    mail_pro = models.EmailField(
+        _("Professional email"),
+        blank=False,
+        max_length=254,
+        default="",
+    )
+    web_site = models.URLField(
+        _("Web site"),
+        blank = True,
+        max_length = 200,
+        default="",
+    )
+    logo = models.URLField(
+        _("Logo"),
+        blank = True,
+        max_length = 200,
+        default="",
+    )
     def __str__(self):
         return self.title
 
