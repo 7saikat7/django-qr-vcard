9dca7aa426ca1de2cfe6e53230ec2131342c44ba
Sam <samysaad01@gmail.com>
2020-09-21 18:20:01 +0200

    add full name attemp to add link to vcf file

--- qr_vcard/admin/vcard.py
+++ qr_vcard/admin/vcard.py
@@ -18,29 +18,9 @@ class VCardAdmin(admin.ModelAdmin):
         'mail_pro',
         'web_site',
         'logo',
+        'get_full_name',
+        'build_vcf',
         )
-
-    # def vcfWriter(organization, first_name, last_name,
-    #           phone_pers, phone_pro, email_pers,
-    #           email_pro, web_site, logo):
-    
-    # name = first_name + ' ' + last_name
-    
-    # vcfLines = []
-    # vcfLines.append('BEGIN:VCARD')
-    # vcfLines.append('VERSION:4.0')
-    # vcfLines.append(f'FN:{name}')
-    # vcfLines.append(f'EMAIL;TYPE=work:{email_pro}')
-    # vcfLines.append(f'EMAIL;TYPE=home:{email_pers}')
-    # vcfLines.append(f'TEL;TYPE=work:{phone_pro}')
-    # vcfLines.append(f'TEL;TYPE=home:{phone_pers}')
-    # vcfLines.append(f'ORG:{organization}')
-    # vcfLines.append(f'URL:{web_site}')
-    # vcfLines.append(f'LOGO:{logo}')
-    # vcfLines.append(f'END:VCARD')
-    # vcfString = ''.join(vcfLines) + '\n'
-    
-    # return vcfString
 
 # Registering interface to model
 admin.site.register(VCard, VCardAdmin)
