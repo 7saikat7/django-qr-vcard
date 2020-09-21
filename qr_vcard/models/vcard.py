d1d3375dd5ac4ff8f1ccd80debe21b25aab4f838
Sam <samysaad01@gmail.com>
2020-09-21 18:20:40 +0200

    add code to generate vcf file from db data

--- qr_vcard/models/vcard.py
+++ qr_vcard/models/vcard.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Value as V
 from django.utils.translation import gettext_lazy as _
 
 
@@ -6,6 +7,7 @@ class VCard(models.Model):
     """
     Vcard model.
     """
+
     organization = models.CharField(
         _("Organization"),
         blank=True,
@@ -60,9 +62,39 @@ class VCard(models.Model):
         max_length = 200,
         default="",
     )
+    title = name_first, name_last, organization
+
+    def get_full_name(self):
+        return self.name_first+' '+self.name_last
+    
+    def get_file_name(self):
+        return self.name_first+'-'+self.name_last+'-'+self.organization
+    
+    def build_vcf(self):
+        vcfLines = [
+            'BEGIN:VCARD',
+            'VERSION:4.0',
+            f'FN:{self.get_full_name}',
+            f'EMAIL;TYPE=work:{self.mail_pro}',
+            f'EMAIL;TYPE=home:{self.mail_pers}',
+            f'TEL;TYPE=work:{self.mobile_pro}',
+            f'TEL;TYPE=home:{self.mobile_pers}',
+            f'ORG:{self.organization}',
+            f'URL:{self.web_site}',
+            f'LOGO:{self.logo}',
+            'END:VCARD',
+            ]
+        with open(f'{self.get_file_name}.vcf',
+                  'w') as f:
+            for elt in vcfLines:
+                f.write(elt)
+                f.write('\n')
+
+    
     def __str__(self):
-        return self.title
+        return str(self.title)
 
     class Meta:
+        unique_together = ('name_first', 'name_last', 'organization')
         verbose_name = _("VCard")
         verbose_name_plural = _("VCards")
