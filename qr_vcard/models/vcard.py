2d92a918be6fa163b371659f29911391f5e171ee
Sam <samysaad01@gmail.com>
2020-09-22 18:58:37 +0200

    add code to generate qr_code (not working properly)

--- qr_vcard/models/vcard.py
+++ qr_vcard/models/vcard.py
@@ -1,6 +1,11 @@
+import os.path
+
+from django.conf import settings
 from django.db import models
-from django.db.models import Value as V
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
+
+import segno
 
 
 class VCard(models.Model):
@@ -62,19 +67,18 @@ class VCard(models.Model):
         max_length = 200,
         default="",
     )
-    title = name_first, name_last, organization
 
     def get_full_name(self):
-        return self.name_first+' '+self.name_last
+        return str(self.name_first)+' '+str(self.name_last)
     
     def get_file_name(self):
-        return self.name_first+'-'+self.name_last+'-'+self.organization
+        return str(self.name_first)+'-'+str(self.name_last)+'-'+str(self.organization)
     
     def build_vcf(self):
         vcfLines = [
             'BEGIN:VCARD',
             'VERSION:4.0',
-            f'FN:{self.get_full_name}',
+            f'FN:{str(self.get_full_name())}',
             f'EMAIL;TYPE=work:{self.mail_pro}',
             f'EMAIL;TYPE=home:{self.mail_pers}',
             f'TEL;TYPE=work:{self.mobile_pro}',
@@ -84,15 +88,45 @@ class VCard(models.Model):
             f'LOGO:{self.logo}',
             'END:VCARD',
             ]
-        with open(f'{self.get_file_name}.vcf',
+        with open(f'{self.get_file_name()}.vcf',
                   'w') as f:
             for elt in vcfLines:
                 f.write(elt)
                 f.write('\n')
 
+    def build_qrcode(self):
+        
+        file_name = self.get_file_name()
+        completeName = settings.MEDIA_ROOT + f"qr_code/{file_name}.txt"
+
+        qrurl = segno.make_qr('a hyperlink to the vcf file', error = "H")    ######
+        qrurl.save(out = f'qr_{completeName}.png', kind = "png", compresslevel = 9, scale = 10, border = 2)
+
+        # open png image to put the logo
+        img = Image.open(f'qr_{completeName}.png')
+        width = img.size
+
+        # How big the logo we want to put in the qr code png
+        logo_size = 100
+
+        # Open the logo image
+        un_logo = Image.open('link to logo image .png')
+
+        # Calculate xmin, ymin, xmax, ymax to put the logo at the center of the qrcode
+        xmin = ymin = int((width / 2) - (logo_size / 2))
+        xmax = ymax = int((width / 2) + (logo_size / 2))
+
+        # resize the logo as calculated
+        un_logo = un_logo.resize((xmax - xmin, ymax - ymin))
+
+        # put the logo in the qr code
+        img.paste(un_logo, (xmin, ymin, xmax, ymax))
+
+        # save the qr_code
+        img.save(f'{completeName}')
     
     def __str__(self):
-        return str(self.title)
+        return str(self.name_first)+'-'+str(self.name_last)+'-'+str(self.organization)
 
     class Meta:
         unique_together = ('name_first', 'name_last', 'organization')
