7af83e3effc1234533a62e608dd239f481c78340
Sam <samysaad01@gmail.com>
2020-09-22 18:55:43 +0200

    sorting imports changed path to open&save

--- qr_vcard/--generate_qr/qrcard.py
+++ qr_vcard/--generate_qr/qrcard.py
@@ -1,7 +1,7 @@
-import segno
-from segno import helpers
 from PIL import Image
 
+import segno
+from segno import helpers
 
 organization = "emencia"
 name_first = "samy"
@@ -14,17 +14,17 @@ web_site = "www.emencia.com"
 logo = ""
 
 qrurl = segno.make_qr('https://www.emencia.com/fr/', error = "H")
-qrurl.save(out = 'django-qr-vcard/qr_vcard/--generate_qr/qr_emencia_url-logo.png', kind = "png", compresslevel = 9, scale = 10, border = 2)
+qrurl.save(out = 'qr_emencia_url-logo.png', kind = "png", compresslevel = 9, scale = 10, border = 2)
 
-# Now open that png image to put the logo
-img = Image.open('/home/sam/Bureau/QRcard/django-qr-vcard/qr_vcard/--generate_qr/qr_emencia_url-logo.png')
+# open png image to put the logo
+img = Image.open('qr_emencia_url-logo.png')
 width, height = img.size
 
 # How big the logo we want to put in the qr code png
 logo_size = 100
 
 # Open the logo image
-un_logo = Image.open('/home/sam/Bureau/QRcard/django-qr-vcard/qr_vcard/--generate_qr/logo.png')
+un_logo = Image.open('logo.png')
 
 # Calculate xmin, ymin, xmax, ymax to put the logo at the center of the qrcode
 xmin = ymin = int((width / 2) - (logo_size / 2))
@@ -37,7 +37,7 @@ un_logo = un_logo.resize((xmax - xmin, ymax - ymin))
 img.paste(un_logo, (xmin, ymin, xmax, ymax))
 
 # save the qr_code
-img.save('/home/sam/Bureau/QRcard/django-qr-vcard/qr_vcard/--generate_qr/qr_emencia_url+logo.png')
+img.save('qr_emencia_url+logo.png')
 
 qr_vcard = helpers.make_vcard(name = f'{name_last};{name_first}',
                               displayname = f'{name_first};{name_last}',
@@ -48,4 +48,4 @@ qr_vcard = helpers.make_vcard(name = f'{name_last};{name_first}',
                               photo_uri = logo,
                               )
 
-qr_vcard.save(out = 'django-qr-vcard/qr_vcard/--generate_qr/qr_vcard-logo.png', kind = "png", compresslevel = 9, scale = 10,border = 2)
+qr_vcard.save(out = 'qr_vcard-logo.png', kind = "png", compresslevel = 9, scale = 10,border = 2)
