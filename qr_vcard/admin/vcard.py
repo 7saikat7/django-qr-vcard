478e9f8b8fd8f22534987a9031d7217b8e9d91c3
Sam <samysaad01@gmail.com>
2020-09-22 18:57:28 +0200

    Failed attempt to build vcf file on demand :(

--- qr_vcard/admin/vcard.py
+++ qr_vcard/admin/vcard.py
@@ -8,10 +8,17 @@ from ..models import VCard
 
 
 class VCardAdmin(admin.ModelAdmin):
+    
+    def make_vcf(modeladmin, request, queryset):
+        VCard.build_vcf
+    make_vcf.short_description = "Make a vcf file."
+    
+    actions = [make_vcf, VCard.build_qrcode]
+    
     list_display = (
+        'name_last',
         'organization',
         'name_first',
-        'name_last',
         'mobile_pers',
         'mobile_pro',
         'mail_pers',
@@ -19,8 +26,20 @@ class VCardAdmin(admin.ModelAdmin):
         'web_site',
         'logo',
         'get_full_name',
-        'build_vcf',
+        'build_vcf'
         )
+    
+    ordering = ('name_last',)
+    search_fields = ('name_last', 'organization')
 
+"""     def Download_vcf(self, request, queryset):
+        from django.http import HttpResponse
+        VCard.build_vcf()
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
+        return response
+
+    Download_vcf.short_description = "Download .vcf file for selected contacts." """
+    
 # Registering interface to model
 admin.site.register(VCard, VCardAdmin)
