e93562ec3193ea66c0b08153d55ef2ddc65cb472
Sam <samysaad01@gmail.com>
2020-09-23 18:08:07 +0200

    Cleaned file

--- qr_vcard/admin/vcard.py
+++ qr_vcard/admin/vcard.py
@@ -3,20 +3,20 @@
 VCard admin interface
 """
 from django.contrib import admin
+from django.http import HttpResponse
 
 from ..models import VCard
 
 
 class VCardAdmin(admin.ModelAdmin):
+    """
+    [summary]
 
-    def make_vcf(modeladmin, request, queryset):
-        VCard.build_vcf
-    make_vcf.short_description = "Make a vcf file."
-
-    actions = [make_vcf, VCard.build_qrcode]
+    Args:
+        admin ([type]): [description]
+    """
 
     list_display = (
-        'get_full_name',
         'name_last',
         'organization',
         'name_first',
@@ -26,21 +26,12 @@ class VCardAdmin(admin.ModelAdmin):
         'mail_pro',
         'web_site',
         'logo',
-        'build_vcf'
+        'build_vcf',
+        'build_qrcode',
         )
 
     ordering = ('name_last',)
     search_fields = ('name_last', 'organization')
-    """
-    def Download_vcf(self, request, queryset):
-        from django.http import HttpResponse
-        VCard.build_vcf()
-        response = HttpResponse(f, content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
-        return response
-
-    Download_vcf.short_description = "Download .vcf file for selected contacts."
-    """
 
 
 # Registering interface to model
