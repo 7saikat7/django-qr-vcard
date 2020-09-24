fe517fabc6b13afd9b229477be798fac66870c01
Sam <samysaad01@gmail.com>
2020-09-24 18:04:42 +0200

    addes static() to urlpatterns (logo, vcf, qrcode)

--- qr_vcard/urls.py
+++ qr_vcard/urls.py
@@ -1,11 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from qr_vcard.views.index import IndexView
-
 
 app_name = 'qr_vcard'
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
