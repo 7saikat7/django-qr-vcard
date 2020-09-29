6918a149c38a4b419a0048816a890e426c5407c6
Sam <samysaad01@gmail.com>
2020-09-29 18:16:46 +0200

    Added instanciation of VCardFactory

--- tests/020_factories/021_vcard.py
+++ tests/020_factories/021_vcard.py
@@ -6,5 +6,14 @@ def test_factory(db):
     """
     Factory should correctly create a new object without any errors
     """
-    instance = VCardFactory(title="foo")
+    instance = VCardFactory(
+        organization="company",
+        name_first="first_name",
+        name_last="last_name",
+        mobile_pers="phone_number",
+        mobile_pro="phone_number",
+        mail_pers="email",
+        mail_pro="company_email",
+        web_site="url",
+    )
     assert instance.title == "foo"
