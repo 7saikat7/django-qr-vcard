d8478e91b7ca5d2ca86ae487b73693d1320de3cf
Sam <samysaad01@gmail.com>
2020-09-29 18:16:01 +0200

    Added Factory attributs

--- qr_vcard/factories/vcard.py
+++ qr_vcard/factories/vcard.py
@@ -8,7 +8,18 @@ class VCardFactory(factory.django.DjangoModelFactory):
     """
     Factory to create instance of a VCard.
     """
-    title = factory.Faker("text", max_nb_chars=42)
+    def fakir(data_type: str, nbr_char: int):
+        return factory.Faker(data_type, max_nb_char=nbr_char)
+
+    organization = fakir("company", 150)
+    name_first = fakir("first_name", 150)
+    name_last = fakir("last_name", 150)
+    mobile_pers = fakir("phone_number", 50)
+    mobile_pro = fakir("phone_number", 50)
+    mail_pers = fakir("email", 254)
+    mail_pro = fakir("company_email", 254)
+    web_site = fakir("url", 200)
+    logo = factory.django.ImageField(color='blue')
 
     class Meta:
         model = VCard
